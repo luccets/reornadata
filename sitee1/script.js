91831d6436e78ea5afe939f70213bbd28dc5b97e
luccets <lucca.vilaca@gmail.com>
2023-05-12 10:36:05 -0300

    retornadata

---
+++ sitee1/script.js
@@ -0,0 +1,17 @@
+//retornando a data
+
+let horas = new Date();
+document.write(horas.getHours());
+document.write("<br><br>");
+
+// retorna minutos
+
+let minutos = new Date();
+document.write(minutos.getMinutes());
+document.write("<br><br>");
+
+//retorna segundos
+
+let segundos = new Date();
+document.write(segundos.getSeconds());
+document.write("<br><br>");
